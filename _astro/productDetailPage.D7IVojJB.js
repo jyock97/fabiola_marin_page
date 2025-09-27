c9258def4ce096a7b04f06913c76921f5a402422
Github Action <github@actions.com>
2025-09-27 23:37:14 +0000

    Build 18066368617

---
+++ _astro/productDetailPage.D7IVojJB.js
@@ -0,0 +1,1 @@
+import{j as t,L as r,B as o}from"./Layout.Cy1YgFyI.js";import"./index.DqJjAbbi.js";const e=()=>t.jsx(r,{children:t.jsx(o,{children:"TEST"})});export{e as default};
--- _astro/productDetailPage.ewAIKf56.js
+++
@@ -1,1 +0,0 @@
-import{j as t,L as r,B as o}from"./Layout.2LK9kTRF.js";import"./index.DqJjAbbi.js";const e=()=>t.jsx(r,{children:t.jsx(o,{children:"TEST"})});export{e as default};
